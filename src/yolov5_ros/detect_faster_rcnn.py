10049b1a10e225dc5b5137e16c048bbe2d8d57cf
Gautam Narayan <98248099+gnarayan1337@users.noreply.github.com>
2024-05-05 22:28:27 -0500

    Create detect_faster_rcnn.py
    Detection script for Faster R-CNN.

---
+++ src/yolov5_ros/detect_faster_rcnn.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import rospy
+import numpy as np
+import cv2
+import torch
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from yolov5_ros.utils import load_faster_rcnn_model
+
+class FasterRCNNDetector:
+    def __init__(self):
+        rospy.init_node('faster_rcnn_detector', anonymous=True)
+
+        self.model = load_faster_rcnn_model()
+        self.bridge = CvBridge()
+        self.camera_topic = rospy.get_param('~camera_topic', '/camera/color/image_raw')
+
+        rospy.Subscriber(self.camera_topic, Image, self.image_callback)
+
+        rospy.loginfo(f"Subscribed to {self.camera_topic}")
+
+    def image_callback(self, msg):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        except CvBridgeError as e:
+            rospy.logerr(e)
+            return
+
+        results = self.model(cv_image)
+        rospy.loginfo(results.pandas().xyxy[0])  # Log results
+
+        cv2.imshow("Faster R-CNN Detections", np.squeeze(results.render()))
+        cv2.waitKey(1)
+
+    def run(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    detector = FasterRCNNDetector()
+    detector.run()
